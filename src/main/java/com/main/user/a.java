4922812eff21a232029dc2264586a341dcae8bef
Seongyu <gcael@naver.com>
2023-11-24 20:17:22 +0900

    ci_test

--- src/main/java/com/main/user/a.java
+++ src/main/java/com/main/user/a.java
@@ -1,9 +1,5 @@
-package com.main.calender;
+package com.main.user;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class a {
 
 }
