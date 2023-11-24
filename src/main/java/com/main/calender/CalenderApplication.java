bfa8411efea17733a8697e5f207943fced7fb26c
Seongyu <gcael@naver.com>
2023-11-24 21:44:51 +0900

    ci_test

--- src/main/java/com/main/calender/CalenderApplication.java
+++ src/main/java/com/main/calender/CalenderApplication.java
@@ -2,12 +2,16 @@ package com.main.calender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class CalenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalenderApplication.class, args);
+
+		SpringApplication app = new SpringApplication(CalenderApplication.class);
+		app.addListeners(new ApplicationPidFileWriter()); // ApplicationPidFileWriter 설정
+		app.run(args);
 	}
 
 }
