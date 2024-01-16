44f88c70179e27c13bd613dcbef966e51a64c70e
Seongyu <gcael@naver.com>
2024-01-17 01:11:17 +0900

    Auditing 기능 추가

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -1,10 +1,10 @@
 package com.example.web;
 
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WebApplication {
 	public static void main(String[] args) {
