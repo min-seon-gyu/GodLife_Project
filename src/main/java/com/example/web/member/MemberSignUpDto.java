e0b0bf6d0d082ce34527daf7ff49c249b48efda8
Seongyu <gcael@naver.com>
2024-01-20 14:40:55 +0900

    회원가입 Dto @Valid 추가

--- src/main/java/com/example/web/member/MemberSignUpDto.java
+++ src/main/java/com/example/web/member/MemberSignUpDto.java
@@ -1,12 +1,22 @@
 package com.example.web.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class MemberSignUpDto {
+    @NotBlank(message = "아이디를 입력해주세요")
+    @Size(min = 8, max = 12, message = "아이디는 8자 이상 12자 이하여야합니다.")
     private String loginId;
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 8, max = 12, message = "비밀번호는 8자 이상 12자 이하여야합니다.")
     private String password;
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
     private String email;
     private String address;
 }
