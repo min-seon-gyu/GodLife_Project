bb566a5637ccf8c1d027566bc72b321c6d2c051e
Seongyu <gcael@naver.com>
2024-02-07 00:43:26 +0900

    기존 ApiController 반복되는 try-catch 제거, ExceptionHandler 수정

--- src/main/java/com/example/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/web/exception/GlobalExceptionHandler.java
@@ -13,12 +13,12 @@ import java.util.stream.Collectors;
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<Object> handleRestApiException(RestApiException e) {
+    public ResponseEntity<ErrorResponse> handleRestApiException(RestApiException e) {
         return handleExceptionInternal(e.getErrorCode(), e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<ErrorResponse.ValidationError> collect =
                 e.getBindingResult()
                         .getFieldErrors()
@@ -29,22 +29,22 @@ public class GlobalExceptionHandler{
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Object> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         return handleExceptionInternal(ErrorCode.METHOD_NOT_ALLOWED, "해당 방식을 지원하지 않습니다.");
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException e) {
+    public ResponseEntity<ErrorResponse> handleNoResourceFoundException(NoResourceFoundException e) {
         return handleExceptionInternal(ErrorCode.NOT_FOUND, "해당 리소스를 찾을 수 없습니다.");
     }
 
-    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode, String message) {
         return ResponseEntity
                 .status(errorCode.getHttpStatus().value())
                 .body(new ErrorResponse(errorCode, message));
     }
 
-    private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message, List<ErrorResponse.ValidationError> errors) {
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode, String message, List<ErrorResponse.ValidationError> errors) {
         return ResponseEntity
                 .status(errorCode.getHttpStatus().value())
                 .body(new ErrorResponse(errorCode, message, errors));
--- src/main/java/com/example/web/member/MemberApiController.java
+++ src/main/java/com/example/web/member/MemberApiController.java
@@ -12,78 +12,37 @@ public class MemberApiController {
     private final MemberService memberService;
     @PostMapping("/member")
     public ResponseEntity signUp(@Valid @RequestBody MemberSignUpDto memberSignUpDto){
-        try {
-            memberService.signUp(memberSignUpDto);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PostMapping("/member/login")
-    public ResponseEntity login(@RequestBody MemberLoginDto memberLoginDto){
-        try {
-            String jwt = memberService.login(memberLoginDto);
-            return new ResponseEntity(jwt, HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.signUp(memberSignUpDto);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/member/logout")
     public ResponseEntity logout(){
-        try {
-            memberService.logout();
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.logout();
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PatchMapping("/member")
     public ResponseEntity update(@RequestBody MemberUpdateDto memberUpdateDto){
-        try {
-            memberService.update(memberUpdateDto);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.update(memberUpdateDto);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @DeleteMapping("/member")
     public ResponseEntity delete(){
-        try {
-            memberService.delete();
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.delete();
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/findLoginId")
     public ResponseEntity findLoginId(@RequestBody MemberFindLoginIdDto memberFindLoginIdDto){
-        try {
-            memberService.findLoginId(memberFindLoginIdDto);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.findLoginId(memberFindLoginIdDto);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @PostMapping("/findPassword")
     public ResponseEntity findPassword(@RequestBody MemberFindPasswordDto memberFindPasswordDto){
-        try {
-            memberService.findPassword(memberFindPasswordDto);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
-        }
+        memberService.findPassword(memberFindPasswordDto);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
