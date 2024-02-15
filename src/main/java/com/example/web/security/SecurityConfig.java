48a906ec261fcc3efc2c647a5db6ae2418ff7f37
Seongyu <gcael@naver.com>
2024-02-16 01:29:30 +0900

    기존 jwt -> session 수정 중

--- src/main/java/com/example/web/security/SecurityConfig.java
+++ src/main/java/com/example/web/security/SecurityConfig.java
@@ -4,15 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,7 +21,8 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SecurityConfig{
     private final AuthenticationConfiguration authenticationConfiguration;
-    private final JwtUtil jwtUtil;
+
+    //private final JwtUtil jwtUtil;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -35,7 +33,35 @@ public class SecurityConfig{
         return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    //Oauth2 적용
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(c -> c.configurationSource(corsConfigurationSource()))
+                .csrf((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                .sessionManagement((auth) ->
+                        auth
+                                .maximumSessions(1)
+                                .maxSessionsPreventsLogin(true))
+                .authorizeHttpRequests(request -> {
+                    //인증이 필요한 요청
+                    //request.requestMatchers(HttpMethod.GET,"/member/create").authenticated();
+                    //ADMIN 권한이 필요한 요청
+                    //request.requestMatchers("/test").hasAnyAuthority("ADMIN");
+                    //그 외 요청
+                    request.anyRequest().permitAll();
+                })
+                .formLogin((formLogin) ->
+                        formLogin
+                                .loginPage("/")
+                                .loginProcessingUrl("/login") // 이 요청이오면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                                .defaultSuccessUrl("/"));
+        return http.build();
+    }
+
+    //JWT 적용
+    /*@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
@@ -45,16 +71,16 @@ public class SecurityConfig{
                 .httpBasic((auth) -> auth.disable())
                 .authorizeHttpRequests(request -> {
                     //인증이 필요한 요청
-                    request.requestMatchers(HttpMethod.PATCH,"/member").authenticated();
-                    request.requestMatchers(HttpMethod.GET,"/member/createFindLoginId").authenticated();
+                    //request.requestMatchers(HttpMethod.GET,"/member/create").authenticated();
                     //ADMIN 권한이 필요한 요청
                     //request.requestMatchers("/test").hasAnyAuthority("ADMIN");
                     //그 외 요청
                     request.anyRequest().permitAll();
                 })
+                .addFilterBefore(new JwtAuthorizationFilter(jwtUtil), JwtAuthenticationFilter.class)
                 .addFilterAt(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
+    }*/
 
 
     @Bean
