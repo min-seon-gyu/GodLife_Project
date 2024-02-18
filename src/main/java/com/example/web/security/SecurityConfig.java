61a67d795f2751b075602cfccaf6f43efa6d57b1
Seongyu <gcael@naver.com>
2024-02-19 01:14:06 +0900

    filterChain 함수 내 formLogin, logout, oauth2Login 구현

--- src/main/java/com/example/web/security/SecurityConfig.java
+++ src/main/java/com/example/web/security/SecurityConfig.java
@@ -1,33 +1,33 @@
 package com.example.web.security;
 
+import com.example.web.security.OAuth2.OAuth2MemberDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity // 필터 체인에 등록
 @RequiredArgsConstructor
 public class SecurityConfig{
-    private final AuthenticationConfiguration authenticationConfiguration;
 
-    //private final JwtUtil jwtUtil;
-
+    private OAuth2MemberDetailsService oAuth2MemberDetailsService;
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
@@ -38,50 +38,34 @@ public class SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(c -> c.configurationSource(corsConfigurationSource()))
-                .csrf((auth) -> auth.disable())
-                .httpBasic((auth) -> auth.disable())
-                .sessionManagement((auth) ->
-                        auth
-                                .maximumSessions(1)
-                                .maxSessionsPreventsLogin(true))
+                .csrf((csrf) -> csrf.disable())
+                .httpBasic((basic) -> basic.disable())
                 .authorizeHttpRequests(request -> {
                     //인증이 필요한 요청
-                    //request.requestMatchers(HttpMethod.GET,"/member/create").authenticated();
+                    request.requestMatchers(HttpMethod.GET,"/createMyPageView").authenticated();
                     //ADMIN 권한이 필요한 요청
                     //request.requestMatchers("/test").hasAnyAuthority("ADMIN");
-                    //그 외 요청
                     request.anyRequest().permitAll();
                 })
-                .formLogin((formLogin) ->
-                        formLogin
-                                .loginPage("/")
-                                .loginProcessingUrl("/login") // 이 요청이오면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
-                                .defaultSuccessUrl("/"));
+                .formLogin((login) -> login
+                        .loginPage("/")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
+                        .failureHandler((request, response, exception) -> {
+                            response.sendRedirect("/?error=true");
+                        }))
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            request.getSession().invalidate();
+                            response.sendRedirect("/");
+                        }))
+                .oauth2Login((oauth2) -> oauth2
+                        .loginPage("/")
+                        .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(oAuth2MemberDetailsService))));
         return http.build();
     }
-
-    //JWT 적용
-    /*@Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(c -> c.configurationSource(corsConfigurationSource()))
-                .csrf((auth) -> auth.disable())
-                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .formLogin((auth) -> auth.disable())
-                .httpBasic((auth) -> auth.disable())
-                .authorizeHttpRequests(request -> {
-                    //인증이 필요한 요청
-                    //request.requestMatchers(HttpMethod.GET,"/member/create").authenticated();
-                    //ADMIN 권한이 필요한 요청
-                    //request.requestMatchers("/test").hasAnyAuthority("ADMIN");
-                    //그 외 요청
-                    request.anyRequest().permitAll();
-                })
-                .addFilterBefore(new JwtAuthorizationFilter(jwtUtil), JwtAuthenticationFilter.class)
-                .addFilterAt(new JwtAuthenticationFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }*/
-
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
