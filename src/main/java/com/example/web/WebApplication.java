b99c873edc3b838a1450d4a7715b480d619c98b7
Seongyu <gcael@naver.com>
2024-02-29 12:23:59 +0900

    @EnableCaching 어노테이션 추가

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -2,9 +2,13 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//엔티티 생성일자/수정일자 어노테이션
 @EnableJpaAuditing
+//캐시 사용 어노테이션
+@EnableCaching
 @SpringBootApplication
 public class WebApplication {
 	public static void main(String[] args) {
