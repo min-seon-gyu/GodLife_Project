95cfed995218c8c6d885214e6d0089ea7a567dab
Seongyu <gcael@naver.com>
2024-02-29 12:53:59 +0900

    기존 메일서비스 외부 api 속도로 인한 전체 서비스 속도 저하 -> 스레드 처리 후 분리, 리팩토링

--- src/main/java/com/example/web/member/MemberApiController.java
+++ src/main/java/com/example/web/member/MemberApiController.java
@@ -1,47 +1,92 @@
 package com.example.web.member;
 
+import com.example.web.common.ExeTimer;
+import com.example.web.common.MailService;
 import com.example.web.exception.ErrorCode;
 import com.example.web.exception.RestApiException;
+import com.example.web.security.MemberDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class MemberApiController {
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
+    private final MemberOAuth2Repository memberOAuth2Repository;
+    private final MailService mailService;
+
+    //회원가입 요청
     @PostMapping("/member")
+    @ExeTimer
+    @ResponseBody
     public ResponseEntity signUp(@Valid @RequestBody MemberSignUpDto memberSignUpDto){
         if(!memberSignUpDto.validPassword()){
             throw new RestApiException(ErrorCode.BAD_REQUEST, "비밀번호 체크가 맞지 않습니다.");
         }
-        memberService.signUp(memberSignUpDto);
+        String key = memberService.signUp(memberSignUpDto);
+        new Thread(() -> mailService.signUp(memberSignUpDto.getEmail(), key)).start();
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    //회원가입 인증 요청
+    @GetMapping("/member/{key}")
+    public String signUpConfirm(@PathVariable("key") String key){
+        memberService.signUpConfirm(key);
+        return "createSignUpConfirm";
+    }
+
+    //회원수정 기본 데이터
+    @GetMapping("/createUpdateView")
+    public String createUpdateView(@AuthenticationPrincipal MemberDetails memberDetails, Model model){
+        Optional<?> findMember = memberDetails.isOAuth2User() ?
+                memberOAuth2Repository.findTop1ByUsername(memberDetails.getUsername()) : memberRepository.findTop1ByUsername(memberDetails.getUsername());
+        Object object = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
+        model.addAttribute("member", object);
+        return "createUpdate";
+    }
+
+    //회원수정 요청
     @PatchMapping("/member")
-    public ResponseEntity update(@RequestBody MemberUpdateDto memberUpdateDto){
-        memberService.update(memberUpdateDto);
+    @ResponseBody
+    public ResponseEntity update(@AuthenticationPrincipal MemberDetails memberDetails,
+                                 @RequestBody MemberUpdateDto memberUpdateDto){
+        memberService.update(memberUpdateDto, memberDetails.getUsername(), memberDetails.isOAuth2User());
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    //회원삭제 요청
     @DeleteMapping("/member")
-    public ResponseEntity delete(){
-        memberService.delete();
+    @ResponseBody
+    public ResponseEntity delete(@AuthenticationPrincipal MemberDetails memberDetails){
+        memberService.delete(memberDetails.getUsername(), memberDetails.isOAuth2User());
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    //아이디찾기 요청
     @PostMapping("/findLoginId")
+    @ResponseBody
     public ResponseEntity findLoginId(@Valid @RequestBody MemberFindUsernameDto memberFindUsernameDto){
-        memberService.findLoginId(memberFindUsernameDto);
+        Optional<Member> findMember = memberRepository.findTop1ByNameAndEmail(memberFindUsernameDto.getName(), memberFindUsernameDto.getEmail());
+        Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
+        new Thread(() -> mailService.findId(memberFindUsernameDto.getEmail(), member.getUsername())).start();
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    //비밀번호찾기 요청
     @PostMapping("/findPassword")
+    @ResponseBody
     public ResponseEntity findPassword(@Valid @RequestBody MemberFindPasswordDto memberFindPasswordDto){
-        memberService.findPassword(memberFindPasswordDto);
+        String password = memberService.findPassword(memberFindPasswordDto);
+        new Thread(() -> mailService.findPassword(memberFindPasswordDto.getEmail(), password)).start();;
         return new ResponseEntity(HttpStatus.OK);
     }
 }
