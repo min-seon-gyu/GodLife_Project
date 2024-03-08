8406b53634582cabadcd68dfad4819d3fbf2fdc0
Seongyu <gcael@naver.com>
2024-03-08 21:52:29 +0900

    레디스 설정 파일 수정

--- src/main/java/com/example/web/redis/RedisConfig.java
+++ src/main/java/com/example/web/redis/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.example.web.redis;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,6 +20,17 @@ public class RedisConfig {
 
     @Value("${spring.redis.port}")
     private int port;
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        RedissonClient redisson = null;
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+        redisson = Redisson.create(config);
+        return redisson;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -30,6 +45,10 @@ public class RedisConfig {
         // Hash를 사용할 경우 시리얼라이저
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // Value를 사용할 경우 시리얼라이저
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class));
 
         return redisTemplate;
     }
