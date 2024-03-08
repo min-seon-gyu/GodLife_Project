5cce0229cff70aef240228f3ffeab18aa4b02cfc
Seongyu <gcael@naver.com>
2024-03-08 21:50:08 +0900

    redissonLock() 추가

--- src/main/java/com/example/web/common/AopService.java
+++ src/main/java/com/example/web/common/AopService.java
@@ -1,23 +1,29 @@
 package com.example.web.common;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class AopService {
 
-    @Pointcut("@annotation(com.example.web.common.ExeTimer)")
-    private void timer(){};
+    private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
+    private static final String REDISSON_LOCK = "LOCK:TEMP";
 
-    @Around("timer()")
+    @Around("@annotation(com.example.web.common.ExeTimer)")
     public void AssumeExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
         StopWatch stopWatch = new StopWatch();
@@ -32,5 +38,29 @@ public class AopService {
         String methodName = signature.getMethod().getName();
 
         log.info("실행 메서드: {}, 실행시간 = {}ms", methodName, totalTimeMillis);
+    }
+
+    @Around("@annotation(com.example.web.common.RedisRock)")
+    public Object redissonLock(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        RedisRock redisRock = method.getAnnotation(RedisRock.class);
+        RLock rLock = redissonClient.getLock(REDISSON_LOCK);
+
+        try {
+            boolean available = rLock.tryLock(redisRock.waitTime(), redisRock.leaseTime(), redisRock.timeUnit());
+            if (!available) {
+                return false;
+            }
+            return aopForTransaction.proceed(joinPoint);
+        } catch (InterruptedException  e) {
+            throw new InterruptedException();
+        } finally {
+            try{
+                rLock.unlock();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
