a704cbc9e30165346ead93f41c31c548ff37b964
Seongyu <gcael@naver.com>
2024-03-08 21:35:39 +0900

    일정 추가, 삭제, 수정 Dto 추가

---
+++ src/main/java/com/example/web/schedule/ScheduleDeleteDto.java
@@ -0,0 +1,9 @@
+package com.example.web.schedule;
+
+import lombok.Data;
+
+@Data
+public class ScheduleDeleteDto {
+    private Long id;
+    private String date;
+}
---
+++ src/main/java/com/example/web/schedule/ScheduleUpdateDto.java
@@ -0,0 +1,13 @@
+package com.example.web.schedule;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ScheduleUpdateDto {
+    private Long id;
+    @NotBlank(message = "일정을 입력해주세요")
+    @Size(min = 2, max = 30, message = "내용은 2자 이상 30자 이하여야합니다.")
+    private String content;
+}
