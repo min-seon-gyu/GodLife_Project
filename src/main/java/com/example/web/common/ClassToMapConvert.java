85a9b090b7cd81b988298b69665acbcd730383ed
Seongyu <gcael@naver.com>
2024-03-12 00:18:25 +0900

    convert() try-catch 추가

--- src/main/java/com/example/web/common/ClassToMapConvert.java
+++ src/main/java/com/example/web/common/ClassToMapConvert.java
@@ -1,17 +1,25 @@
 package com.example.web.common;
 
+import com.example.web.exception.ErrorCode;
+import com.example.web.exception.RestApiException;
+
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ClassToMapConvert {
-    public static Map<Object, Object> convert(Object object) throws IllegalAccessException {
-        Field[] fields = object.getClass().getDeclaredFields();
-        Map<Object, Object> result = new HashMap<>();
-        for(int i = 0 ; i < fields.length ; i++){
-            fields[i].setAccessible(true);
-            result.put(fields[i].getName(), fields[i].get(object));
+    public static Map<Object, Object> convert(Object object){
+        try{
+            Field[] fields = object.getClass().getDeclaredFields();
+            Map<Object, Object> result = new HashMap<>();
+            for(int i = 0 ; i < fields.length ; i++){
+                fields[i].setAccessible(true);
+                result.put(fields[i].getName(), fields[i].get(object));
+            }
+            return result;
+        }catch (IllegalAccessException e){
+            e.printStackTrace();
+            throw new RestApiException(ErrorCode.INTERNAL_SERVER_ERROR, "Map 타입 변환에 실패했습니다.");
         }
-        return result;
     }
 }
