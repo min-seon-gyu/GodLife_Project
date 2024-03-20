fa0b19ade8e449fdce7d933defbc9ce183661c47
Seongyu <gcael@naver.com>
2024-03-20 23:02:36 +0900

    OAuth2 엔티티 삭제

--- src/main/java/com/example/web/member/MemberOAuth2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.web.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberOAuth2Repository extends JpaRepository<MemberOAuth2, Long> {
-    Optional<MemberOAuth2> findTop1ByUsername(String username);
-}
