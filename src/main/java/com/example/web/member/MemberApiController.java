a665e3e57970f8fb12f40482433d6ef8b8cf6aca
Seongyu <gcael@naver.com>
2024-03-20 23:11:13 +0900

    username -> id 로 조건변경

--- src/main/java/com/example/web/member/MemberApiController.java
+++ src/main/java/com/example/web/member/MemberApiController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -36,7 +35,7 @@ public class MemberApiController {
     @PatchMapping("/member")
     public ResponseEntity update(@AuthenticationPrincipal MemberDetails memberDetails,
                                  @RequestBody MemberUpdateDto memberUpdateDto){
-        memberService.update(memberUpdateDto, memberDetails.getUsername(), memberDetails.isOAuth2User());
+        memberService.update(memberUpdateDto, memberDetails.getId());
         return new ResponseEntity(HttpStatus.OK);
     }
 
@@ -45,21 +44,21 @@ public class MemberApiController {
     public ResponseEntity updatePassword(@AuthenticationPrincipal MemberDetails memberDetails,
                                          @Valid @RequestBody MemberUpdatePasswordDto memberUpdatePasswordDto){
         memberUpdatePasswordDto.validPassword();
-        memberService.updatePassword(memberUpdatePasswordDto, memberDetails.getUsername());
+        memberService.updatePassword(memberUpdatePasswordDto, memberDetails.getId());
         return new ResponseEntity(HttpStatus.OK);
     }
 
     //회원삭제 요청
     @DeleteMapping("/member")
     public ResponseEntity delete(@AuthenticationPrincipal MemberDetails memberDetails){
-        memberService.delete(memberDetails.getUsername(), memberDetails.isOAuth2User());
+        memberService.delete(memberDetails.getId());
         return new ResponseEntity(HttpStatus.OK);
     }
 
     //아이디찾기 요청
     @PostMapping("/findLoginId")
     public ResponseEntity findLoginId(@Valid @RequestBody MemberFindUsernameDto memberFindUsernameDto){
-        Optional<Member> findMember = memberRepository.findTop1ByNameAndEmail(memberFindUsernameDto.getName(), memberFindUsernameDto.getEmail());
+        Optional<Member> findMember = memberRepository.findTop1ByNameAndEmailAndIsOAuth(memberFindUsernameDto.getName(), memberFindUsernameDto.getEmail(), false);
         Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
         new Thread(() -> mailService.findId(memberFindUsernameDto.getEmail(), member.getUsername())).start();
         return new ResponseEntity(HttpStatus.OK);
--- src/main/java/com/example/web/member/MemberService.java
+++ src/main/java/com/example/web/member/MemberService.java
@@ -1,15 +1,12 @@
 package com.example.web.member;
 
-import com.example.web.common.ClassToMapConvert;
 import com.example.web.exception.ErrorCode;
 import com.example.web.exception.RestApiException;
-import com.example.web.redis.SignUpService;
 import com.example.web.security.PasswordService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Map;
 import java.util.Optional;
 
@@ -18,14 +15,11 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-    private final MemberOAuth2Repository memberOAuth2Repository;
     private final PasswordService passwordService;
     private final String SIGN_KEY = "SIGN_KEY_";
 
     public String signUp(MemberSignUpDto memberSignUpDto){
-        log.info("회원가입 요청 - id : {}, name : {}, email : {}", memberSignUpDto.getUsername(), memberSignUpDto.getName(), memberSignUpDto.getEmail());
         if(validationCheck(memberSignUpDto)){
-            log.info("회원가입 요청 실패- id : {}, name : {}, email : {}, {}", memberSignUpDto.getUsername(), memberSignUpDto.getName(), memberSignUpDto.getEmail(), "이미 회원가입 했습니다.");
             throw new RestApiException(ErrorCode.BAD_REQUEST, "이미 가입된 회원정보입니다.");
         }
         return SIGN_KEY + memberSignUpDto.getEmail() + "_" + memberSignUpDto.getEmail().hashCode();
@@ -39,45 +33,35 @@ public class MemberService {
                 .name((String) data.get("name"))
                 .password(passwordService.encode((String) data.get("password")))
                 .role(MemberRole.USER)
+                .isOAuth(false)
                 .build();
         memberRepository.save(member);
     }
 
     @Transactional
-    public void update(MemberUpdateDto memberUpdateDto, String username, boolean isOAuth2User) {
-        if(isOAuth2User){
-            Optional<MemberOAuth2> findMember = memberOAuth2Repository.findTop1ByUsername(username);
-            MemberOAuth2 member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
-            member.update(memberUpdateDto);
-        }else{
-            Optional<Member> findMember = memberRepository.findTop1ByUsername(username);
-            Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
-            member.update(memberUpdateDto);
-        }
+    public void update(MemberUpdateDto memberUpdateDto, Long id) {
+        Optional<Member> findMember = memberRepository.findById(id);
+        Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
+        member.update(memberUpdateDto);
     }
 
     @Transactional
-    public void updatePassword(MemberUpdatePasswordDto memberUpdatePasswordDto, String username){
-        Optional<Member> findMember = memberRepository.findTop1ByUsername(username);
+    public void updatePassword(MemberUpdatePasswordDto memberUpdatePasswordDto, Long id){
+        Optional<Member> findMember = memberRepository.findById(id);
         Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
         member.changePassword(passwordService.encode(memberUpdatePasswordDto.getPassword()));
     }
 
     @Transactional
-    public void delete(String username, boolean isOAuth2User) {
-        if(isOAuth2User){
-            Optional<MemberOAuth2> findMember = memberOAuth2Repository.findTop1ByUsername(username);
-            memberOAuth2Repository.delete(findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다.")));
-        }else{
-            Optional<Member> findMember = memberRepository.findTop1ByUsername(username);
-            memberRepository.delete(findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다.")));
-        }
+    public void delete(Long id) {
+        Optional<Member> findMember = memberRepository.findById(id);
+        memberRepository.delete(findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다.")));
     }
 
     @Transactional
     public String findPassword(MemberFindPasswordDto memberFindPasswordDto) {
         log.info("비밀번호 찾기 요청 - id : {}, name : {}, email : {}", memberFindPasswordDto.getUsername(), memberFindPasswordDto.getName(), memberFindPasswordDto.getEmail());
-        Optional<Member> findMember = memberRepository.findTop1ByUsernameAndNameAndEmail(memberFindPasswordDto.getUsername(), memberFindPasswordDto.getName(), memberFindPasswordDto.getEmail());
+        Optional<Member> findMember = memberRepository.findTop1ByUsernameAndNameAndEmailAndIsOAuth(memberFindPasswordDto.getUsername(), memberFindPasswordDto.getName(), memberFindPasswordDto.getEmail(), false);
         Member member = findMember.orElseThrow(() -> new RestApiException(ErrorCode.BAD_REQUEST, "해당하는 회원이 없습니다."));
         String password = passwordService.getRandom();
         member.changePassword(passwordService.encode(password));
@@ -87,6 +71,6 @@ public class MemberService {
 
 
     private boolean validationCheck(MemberSignUpDto memberSignUpDto){
-        return memberRepository.existsByUsername(memberSignUpDto.getUsername()) || memberRepository.existsByEmail(memberSignUpDto.getEmail());
+        return memberRepository.existsByUsername(memberSignUpDto.getUsername()) || memberRepository.existsByEmailAndIsOAuth(memberSignUpDto.getEmail(), false);
     }
 }
