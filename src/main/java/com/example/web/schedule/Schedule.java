50ba2fb3a3067cb2e4746a3710921a13bfd9f780
Seongyu <gcael@naver.com>
2024-03-20 23:07:33 +0900

    Schedule 내부 Member엔티티 추가

--- src/main/java/com/example/web/schedule/Schedule.java
+++ src/main/java/com/example/web/schedule/Schedule.java
@@ -1,6 +1,7 @@
 package com.example.web.schedule;
 
 import com.example.web.common.JpaBaseEntity;
+import com.example.web.member.Member;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
@@ -16,15 +17,18 @@ public class Schedule extends JpaBaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "SCHEDULE_ID")
     private Long id;
-    private String username;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
     private String content;
     private boolean status;
     @Temporal(TemporalType.DATE)
     private LocalDate localDate;
 
     @Builder
-    public Schedule(String username, String content, LocalDate localDate) {
-        this.username = username;
+    public Schedule(Member member, String content, LocalDate localDate) {
+        this.member = member;
         this.content = content;
         this.localDate = localDate;
     }
