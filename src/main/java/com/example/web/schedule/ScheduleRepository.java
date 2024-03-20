15a2cf35be75237983a706a70fd64e9b7a349a24
Seongyu <gcael@naver.com>
2024-03-20 23:12:00 +0900

    @query 추가

--- src/main/java/com/example/web/schedule/ScheduleRepository.java
+++ src/main/java/com/example/web/schedule/ScheduleRepository.java
@@ -1,15 +1,22 @@
 package com.example.web.schedule;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    List<Schedule> findByUsernameAndLocalDate(String username, LocalDate localDate);
+    @Query("select s from Schedule s join fetch s.member m where s.localDate = :localDate and m.id = :id")
+    List<Schedule> findByMemberIdAndLocalDate(@Param("id") Long id, @Param("localDate") LocalDate localDate);
 
-    Long countByUsernameAndLocalDate(String username, LocalDate localDate);
+    @Query("select COUNT(*) from Schedule s join s.member m where s.localDate = :localDate and m.id = :id")
+    Long countByMemberIdAndLocalDate(@Param("id") Long id, @Param("localDate") LocalDate localDate);
 
-    Long deleteByUsernameAndLocalDate(String username, LocalDate localDate);
+    @Query("select s from Schedule s join fetch s.member m where m.id = :id and s.content like %:content% order by s.localDate")
+    Slice<Schedule> findByMemberIdAndContent(Pageable pageable, @Param("id") Long memberId, @Param("content") String content);
 }
