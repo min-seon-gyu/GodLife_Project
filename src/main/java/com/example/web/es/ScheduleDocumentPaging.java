e25f59e42e09ca9d059ba4d5f8c1d0705c07b585
Seongyu <gcael@naver.com>
2024-04-01 01:53:31 +0900

    currentIndex 변수 추가

--- src/main/java/com/example/web/es/ScheduleDocumentPaging.java
+++ src/main/java/com/example/web/es/ScheduleDocumentPaging.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class ScheduleDocumentPaging {
     private List<ScheduleDocumentResponse> lst;
     private boolean hasNext;
+    private int currentIndex;
     public ScheduleDocumentPaging(List<ScheduleDocumentResponse> lst){
         this.lst = lst;
     }
