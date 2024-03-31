9c43ffe68623f12ac870c2803619f54b8ba7b774
Seongyu <gcael@naver.com>
2024-04-01 02:05:15 +0900

    엘라스틱서치를 활용한 검색 기능 및 페이징 처리 구현

--- src/main/java/com/example/web/schedule/ScheduleController.java
+++ src/main/java/com/example/web/schedule/ScheduleController.java
@@ -1,28 +1,40 @@
 package com.example.web.schedule;
 
+import com.example.web.es.ScheduleDocument;
+import com.example.web.es.ScheduleDocumentPaging;
+import com.example.web.es.ScheduleDocumentRepository;
+import com.example.web.es.ScheduleDocumentResponse;
 import com.example.web.redis.PostService;
 import com.example.web.security.MemberDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class ScheduleController {
 
+    private final int pageSize = 5;
+    private final String sortProps = "localDate";
     private final ScheduleService scheduleService;
     private final PostService postService;
+    private final ScheduleDocumentRepository scheduleDocumentRepository;
 
     @PostMapping("/schedule")
-    public ResponseEntity add(@AuthenticationPrincipal MemberDetails memberDetails,
+    public ResponseEntity<Long> add(@AuthenticationPrincipal MemberDetails memberDetails,
                                @Valid @RequestBody ScheduleAddDto scheduleAddDto){
         postService.decreaseWriteCount(scheduleAddDto.getDate()+ "_" + memberDetails.getId());
-        scheduleService.post(memberDetails.getId(), scheduleAddDto);
-        return new ResponseEntity(HttpStatus.OK);
+        Long id = scheduleService.post(memberDetails.getId(), scheduleAddDto);
+        return ResponseEntity.ok(id);
     }
 
     @PostMapping("/schedule/changeStatus")
@@ -33,9 +45,9 @@ public class ScheduleController {
     }
 
     @PatchMapping("/schedule")
-    public ResponseEntity update(@Valid @RequestBody ScheduleUpdateDto scheduleUpdateDto){
-        scheduleService.update(scheduleUpdateDto);
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity<Long> update(@Valid @RequestBody ScheduleUpdateDto scheduleUpdateDto){
+        Long id = scheduleService.update(scheduleUpdateDto);
+        return ResponseEntity.ok(id);
     }
 
     @DeleteMapping("/schedule")
@@ -44,5 +56,21 @@ public class ScheduleController {
         postService.incrementWriteCount(scheduleDeleteDto.getDate()+ "_" + memberDetails.getId());
         scheduleService.delete(scheduleDeleteDto.getId());
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/search_schedule/{content}/{pageIndex}")
+    public ScheduleDocumentPaging find(@AuthenticationPrincipal MemberDetails memberDetails,
+                                       @PathVariable(value = "content", required = false) String content,
+                                       @PathVariable(value = "pageIndex", required = false) int pageIndex){
+
+        PageRequest pageRequest = PageRequest.of(pageIndex, pageSize, Sort.by(sortProps));
+        Slice<ScheduleDocument> findScheduleDocument = scheduleDocumentRepository.findByContentAndMemberId(pageRequest, content, memberDetails.getId());
+        List<ScheduleDocumentResponse> result = findScheduleDocument.getContent().stream().map((s) -> new ScheduleDocumentResponse(s)).toList();
+
+        return ScheduleDocumentPaging.builder().
+                lst(result).
+                nextIndex(findScheduleDocument.getNumber() + 1).
+                hasNext(findScheduleDocument.hasNext()).
+                build();
     }
 }
