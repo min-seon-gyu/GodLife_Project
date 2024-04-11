a44f6675440f7820fd3e626462ac036e2c2aebd8
Seongyu <gcael@naver.com>
2024-04-11 17:20:08 +0900

    주석삭제 및 @EnableCaching 삭제

--- src/main/java/com/example/web/WebApplication.java
+++ src/main/java/com/example/web/WebApplication.java
@@ -2,13 +2,9 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//엔티티 생성일자/수정일자 어노테이션
 @EnableJpaAuditing
-//캐시 사용 어노테이션
-@EnableCaching
 @SpringBootApplication
 public class WebApplication {
 	public static void main(String[] args) {
