d5e0287a1bf92c43da273eb5ea9aa5247159ec88
Seongyu <gcael@naver.com>
2024-05-04 21:52:37 +0900

    포인트 추가

--- src/main/java/com/example/web/member/MemberRepository.java
+++ src/main/java/com/example/web/member/MemberRepository.java
@@ -1,11 +1,16 @@
 package com.example.web.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmailAndIsOAuth(String email, boolean isOAuth);
+    @Query("select point from Member m where m.username = :username")
+    Optional<Long> getPoint(@Param("username") String username);
     Optional<Member> findTop1ByNameAndEmailAndIsOAuth(String name, String email, boolean isOAuth);
     Optional<Member> findTop1ByUsernameAndNameAndEmailAndIsOAuth(String username, String name, String email, boolean isOAuth);
     Optional<Member> findTop1ByUsername(String username);
