84730f852a3a84d69fbccd515c5ebe9ff50dbdf7
Seongyu <gcael@naver.com>
2024-05-06 05:47:02 +0900

    멤버 엔티티 쿠폰, 아이템 추가

--- src/main/java/com/example/web/member/Member.java
+++ src/main/java/com/example/web/member/Member.java
@@ -1,12 +1,18 @@
 package com.example.web.member;
 
 import com.example.web.common.JpaBaseEntity;
+import com.example.web.exception.ErrorCode;
+import com.example.web.exception.RestApiException;
+import com.example.web.item.Item;
+import com.example.web.memberCoupon.MemberCoupon;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,6 +23,10 @@ public class Member extends JpaBaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MEMBER_ID")
     private Long id;
+    @OneToMany(mappedBy = "member")
+    private List<Item> items = new ArrayList<>();
+    @OneToMany(mappedBy = "member")
+    private List<MemberCoupon> coupons = new ArrayList<>();
     @Column(unique = true)
     private String username;
     private String password;
@@ -50,5 +60,29 @@ public class Member extends JpaBaseEntity {
 
     public void update(MemberUpdateDto memberUpdateDto){
         this.address = memberUpdateDto.getAddress();
+    }
+
+    public void buy(Long point){
+        validate(point);
+        this.point -= point;
+    }
+
+    public void setItems(Item... items){
+        for (Item item : items) {
+            this.items.add(item);
+            item.setMember(this);
+        }
+    }
+
+    public void setCoupons(MemberCoupon... coupons){
+        for (MemberCoupon coupon : coupons) {
+            this.coupons.add(coupon);
+            coupon.setMember(this);
+        }
+    }
+
+    private void validate(Long point) {
+        Long restPoint = this.point - point;
+        if(restPoint < 0) throw new RestApiException(ErrorCode.BAD_REQUEST, "포인트가 부족합니다.");
     }
 }
