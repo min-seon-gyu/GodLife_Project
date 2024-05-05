4ce2edee9f9c64e062a229f5200c1b941abf414b
Seongyu <gcael@naver.com>
2024-05-06 05:46:18 +0900

    findMemberById 함수 구현

--- src/main/java/com/example/web/member/MemberRepository.java
+++ src/main/java/com/example/web/member/MemberRepository.java
@@ -3,14 +3,13 @@ package com.example.web.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmailAndIsOAuth(String email, boolean isOAuth);
-    @Query("select point from Member m where m.username = :username")
-    Optional<Long> getPoint(@Param("username") String username);
+    @Query("select m from Member m left join fetch m.items i where m.id = :id")
+    Optional<Member> findMemberById(@Param("id") Long id);
     Optional<Member> findTop1ByNameAndEmailAndIsOAuth(String name, String email, boolean isOAuth);
     Optional<Member> findTop1ByUsernameAndNameAndEmailAndIsOAuth(String username, String name, String email, boolean isOAuth);
     Optional<Member> findTop1ByUsername(String username);
