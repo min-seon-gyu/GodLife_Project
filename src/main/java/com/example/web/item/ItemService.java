223477b54856f8dbee0142e618cdc9258bbbc643
Seongyu <gcael@naver.com>
2024-05-08 02:41:14 +0900

    ItemService 추가

--- src/main/java/com/example/web/item/ItemService.java
+++ src/main/java/com/example/web/item/ItemService.java
@@ -1,7 +1,32 @@
 package com.example.web.item;
 
+import com.example.web.member.Member;
+import com.example.web.order.OrderAddDto;
+import com.example.web.product.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long update(OrderAddDto orderAddDto,  Member member, Product product){
+        Item item = itemRepository.findTop1ByProductIdAndMemberId(product.getId(), member.getId()).orElseGet(() -> getNewItem(member, product));
+        item.addQuantity(orderAddDto.getTotalQuantity());
+        return item.getId();
+    }
+
+    private Item getNewItem(Member member, Product product){
+        Item item = Item.builder()
+                .member(member)
+                .product(product)
+                .quantity(0l)
+                .build();
+        itemRepository.save(item);
+        return item;
+    }
 }
