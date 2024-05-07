c9c9092c2c28cde77e046dcdb668e1ca64f20795
Seongyu <gcael@naver.com>
2024-05-08 02:04:40 +0900

    import 정리

--- src/main/java/com/example/web/schedule/ScheduleAddDto.java
+++ src/main/java/com/example/web/schedule/ScheduleAddDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 import java.time.LocalDate;
 
 @Data
--- src/main/java/com/example/web/schedule/ScheduleRepository.java
+++ src/main/java/com/example/web/schedule/ScheduleRepository.java
@@ -3,7 +3,6 @@ package com.example.web.schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDate;
 import java.util.List;
 
